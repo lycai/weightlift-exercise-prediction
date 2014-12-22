8d37cce31de64049a46b8cb4411e3f9850af4de0
lycai <ly3cai@uwaterloo.ca>
2014-12-21 19:28:55 -0500

    New index and data files.

--- pml-main.R
+++ pml-main.R
@@ -7,6 +7,28 @@ cat("R code for predicting exercise type from WLE data\n\n")
 #   Qualitative Activity Recognition of Weight Lifting Exercises.
 #   Proceedings of 4th International Conference in Cooperation with SIGCHI (Augmented Human '13).
 #   Stuttgart, Germany: ACM SIGCHI, 2013.
+#
+# Usage:
+# 1. Place csv training and test data into "pml-training.csv" and "pml-testing.csv" respectively,
+#     and place files into working directory.
+#     Alternatively, set pml.trainData and pml.testData variables to be the paths to the two files.
+# 2. Call the script with `source('path-to-script-file/pml-main.R')`
+# 3. ????
+# 4. PROFIT!!!
+# 5. You were done at step 2.
+#
+# Notes:
+#   The reason why I didn't make this into a function was because I wanted easy access to both
+#   the model and the results, to more easily analyse the data. If required, a simple function
+#   wrapper with pml.[train/test]Data as arguments could be easily added.
+
+# Setting default file paths/names
+if (!exists("pml.trainData")) {
+  pml.trainData = "pml-training.csv"
+}
+if (!exists("pml.testData")) {
+  pml.testData = "pml-testing.csv"
+}
 
 set.seed(1)
 
@@ -15,13 +37,13 @@ library(randomForest)
 
 cat("Done!\nLoading and optimising training data... ")
 # Read the first 5 rows and determine good columns (no NAs or timestamps, etc)
-pml.5rows <- read.csv("pml-training.csv", nrows = 5)
+pml.5rows <- read.csv(pml.trainData, nrows = 5)
 pml.goodcols <- sapply(pml.5rows, function(x) {!any(is.na(x))})
 pml.goodcols[c("X", "user_name", "raw_timestamp_part_1", "raw_timestamp_part_2",
                "cvtd_timestamp")] <- FALSE
 
 # Now apply good columns to the rest of the data
-pml.train <- read.csv("pml-training.csv")[, pml.goodcols]
+pml.train <- read.csv(pml.trainData)[, pml.goodcols]
 
 cat("Done!\nTraining random forest model... ")
 if (!exists("pml.rf")) {
@@ -30,7 +52,7 @@ if (!exists("pml.rf")) {
 cat("Done!\n")
 
 cat("Loading test data... ")
-pml.test <- read.csv("pml-testing.csv")[, pml.goodcols]
+pml.test <- read.csv(pml.testData)[, pml.goodcols]
 # Append to training data
 if (any(colnames(pml.test) == "problem_id")) {
   colnames(pml.test)[colnames(pml.test) == "problem_id"] <- "classe"
